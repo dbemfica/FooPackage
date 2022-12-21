59e5df8c0d1cb860c86ddf48592046ce0d5c7098
Diogo Bemfica <diogo.bemfica@apps4business.com.br>
2022-12-21 15:53:24 -0300

    commit

---
+++ Sources/FooPackage/TesteController.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Diogo Bemfica on 21/12/22.
+//
+
+import UIKit
+
+class TesteController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+}
