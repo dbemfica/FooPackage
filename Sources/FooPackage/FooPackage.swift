782fc4603985c3c9b138c44464717884accd8cec
Diogo Bemfica <diogo.bemfica@apps4business.com.br>
2022-12-21 16:03:54 -0300

    commit

--- Sources/FooPackage/FooPackage.swift
+++ Sources/FooPackage/FooPackage.swift
@@ -1,6 +1,14 @@
+import UIKit
+
 public struct FooPackage {
     public private(set) var text = "Hello, World!"
 
     public init() {
     }
+    
+    public func getController() -> UIViewController {
+        let controller = TesteController()
+        return controller
+    }
+    
 }
